a5d619fde44b39017cfebdf7e6520d39b98c43aa
MaximePeterburg <getmax4444@gmail.com>
2023-10-03 21:21:37 +0300

    minor typeo fix

--- src/projects-data.ts
+++ src/projects-data.ts
@@ -1,10 +1,12 @@
 export const PROJECTS_DATA = [
   {
     title: 'project-1',
-    description: 'Описание 1'
+    description: 'Описание 1',
+    tasks: []
   },
   {
     title: 'project-2',
-    description: 'Описание 2'
+    description: 'Описание 2',
+    tasks: []
   }
 ];
