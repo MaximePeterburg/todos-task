d5087f7281f75f0f5827a3e1ecf39ca963119eb3
MaximePeterburg <getmax4444@gmail.com>
2023-10-03 21:21:08 +0300

    added sagas to the project

---
+++ src/store/projects/projects.saga.ts
@@ -0,0 +1,21 @@
+import { all, call, put, takeLatest } from 'typed-redux-saga';
+import { getProjectsAndDocuments } from '../../utils/firebase/firebase.utils';
+import { fetchProjectsFailed, fetchProjectsSuccess } from './projects.action';
+import { PROJECTS_ACTION_TYPES } from './projects.types';
+
+export function* fetchProjectsAsync() {
+  try {
+    const projects = yield* call(getProjectsAndDocuments);
+    yield* put(fetchProjectsSuccess(projects));
+  } catch (error) {
+    yield* put(fetchProjectsFailed(error as Error));
+  }
+}
+
+export function* onFetchProjects() {
+  yield* takeLatest(PROJECTS_ACTION_TYPES.FETCH_CATEGORIES_START, fetchProjectsAsync);
+}
+
+export function* projectsSaga() {
+  yield* all([call(onFetchProjects)]);
+}
