b11d05a416e574a0f876defeddf28a8fb84754eb
MaximePeterburg <getmax4444@gmail.com>
2023-10-06 15:06:55 +0300

    added modal to create task

---
+++ src/components/tasks-column.component.tsx
@@ -0,0 +1,54 @@
+import { MouseEventHandler, useRef } from 'react';
+import css from 'styled-jsx/macro';
+import { TASK_STATUS } from '../store/projects/projects.types';
+type TasksColumnProps = {
+  taskStatus: TASK_STATUS;
+};
+const TasksColumn = ({ taskStatus }: TasksColumnProps) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const handleButtonClick = () => {
+    modalRef.current?.showModal();
+  };
+  const handleBackdropClick: MouseEventHandler<HTMLDialogElement> = (e) => {
+    if (modalRef.current) {
+      const modalDimensions = modalRef.current.getBoundingClientRect();
+      if (
+        e.clientX < modalDimensions.left ||
+        e.clientX > modalDimensions.right ||
+        e.clientY < modalDimensions.top ||
+        e.clientY > modalDimensions.bottom
+      ) {
+        modalRef.current.close();
+      }
+    }
+  };
+  const { className, styles } = css.resolve`
+    div {
+      min-width: 15rem;
+    }
+    button {
+      width: 100%;
+      padding: 0.5rem 1rem;
+      border-radius: 0.25rem;
+      display: flex;
+    }
+    button:hover {
+      background: linear-gradient(135deg, #ffd700, #ffa500, #ff6347, #ff4500);
+    }
+  `;
+  return (
+    <div>
+      <header>
+        <h3>{taskStatus}</h3>
+      </header>
+      <div className={className}></div>
+      <button className={className} onClick={handleButtonClick}>
+        + Добавить
+      </button>
+      <dialog ref={modalRef} onClick={handleBackdropClick}></dialog>
+      {styles}
+    </div>
+  );
+};
+
+export default TasksColumn;
