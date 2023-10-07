4f2c5643bf460297212a8ae5df02c2e04ca79298
MaximePeterburg <getmax4444@gmail.com>
2023-10-07 22:06:34 +0300

    added link to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
+import css from 'styled-jsx/macro';
 import ProjectChoice from '../components/project-choice.component';
 import Project from './project';
 
 const Home = () => {
+  const { className, styles } = css.resolve`
+    div {
+      display: flex;
+      justify-content: center;
+      padding: 2rem;
+      font-weight: 600;
+    }
+  `;
   return (
-    <Routes>
-      <Route index element={<ProjectChoice />}></Route>
-      <Route path=':projectId' element={<Project />} />
-    </Routes>
+    <>
+      <div className={className}>
+        <Link to='/'> 🏚️ На Главную</Link>
+        {styles}
+      </div>
+      <Routes>
+        <Route index element={<ProjectChoice />}></Route>
+        <Route path=':projectId' element={<Project />} />
+      </Routes>
+    </>
   );
 };
 
